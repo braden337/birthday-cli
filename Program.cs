130df8fa4efbb4f609d418e8a16858bbedba9544
Braden Jonsson <braden337@gmail.com>
2021-02-21 17:15:51 -0600

    Simplify escape sequences

--- Program.cs
+++ Program.cs
@@ -6,34 +6,38 @@ namespace Birthday
     {
         static void Main(string[] args)
         {
-            Console.Write("\x1b[2J\x1b[H");
+            var clearScreen = "\x1b[2J\x1b[H";
+            var clearLine = "\x1b[K";
+            var upLine = "\x1b[F";
+
+            Console.Write(clearScreen);
 
             DateTime birthday;
 
             while (true)
             {
-                Console.Write("\x1b[KWhen's your birthday? ('e' to exit) ");
+                Console.Write("{0}When's your birthday? ('e' to exit) ", clearLine);
                 var input = Console.ReadLine();
 
                 if (DateTime.TryParse(input, out birthday))
                 {
                     var today = DateTime.Now;
-                    var difference = today - birthday;
-                    var age = difference.Duration().Days / 365.25;
+                    TimeSpan difference = today - birthday;
+                    var age = (int)(difference.Duration().Days / 365.25);
 
                     Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.Write("\x1b[KYou are {0} years old.\x1b[F", (int)age);
+                    Console.Write("{0}You are {1} years old.{2}", clearLine, age, upLine);
                     Console.ResetColor();
                 }
                 else if (input == "e")
                 {
-                    Console.Write("\x1b[2J\x1b[H");
+                    Console.Write(clearScreen);
                     Environment.Exit(0);
                 }
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write("\x1b[KThat's not a valid date\x1b[F");
+                    Console.Write("{0}hat's not a valid date{1}", clearLine, upLine);
                     Console.ResetColor();
                 }
             }
